defc796cdafaaac4d76bd7a80d313d9661e01816
saul gonzo <94.gonzlezsaul@gmail.com>
2023-01-06 19:00:06 -0500

    URL params in Loader

--- myViteReactApp/src/routes/root.jsx
+++ myViteReactApp/src/routes/root.jsx
@@ -1,5 +1,10 @@
-import { Outlet, Link, useLoaderData } from 'react-router-dom'
-import { getContacts } from '../contacts'
+import { Outlet, Link, useLoaderData, Form } from 'react-router-dom'
+import { getContacts, createContact } from '../contacts'
+
+export async function action() {
+  const contact = await createContact()
+  return { contact }
+}
 
 export async function loader() {
   const contacts = await getContacts()
@@ -12,6 +17,7 @@ export default function Root() {
     <>
       <div id='sidebar'>
         <h1>React Router Contacts</h1>
+
         <div>
           <form id='search-form' role='search'>
             <input
@@ -24,9 +30,9 @@ export default function Root() {
             <div id='search-spinner' aria-hidden hidden={true} />
             <div className='sr-only' aria-live='polite'></div>
           </form>
-          <form method='post'>
+          <Form method='post'>
             <button type='submit'>New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
