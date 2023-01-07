1c993e9fa25458455c8091d7494707bb800a4f67
saul gonzo <94.gonzlezsaul@gmail.com>
2023-01-07 16:27:23 -0500

    react router tutorial finsihed
    use this and docs to continue exploring react router

--- myViteReactApp/src/routes/destroy.jsx
+++ myViteReactApp/src/routes/destroy.jsx
@@ -2,7 +2,6 @@ import { redirect } from 'react-router-dom'
 import { deleteContact } from '../contacts'
 
 export async function action({ request, params }) {
-  throw new Error('oh dang!')
   await deleteContact(params.contactId)
 
   return redirect('/')
