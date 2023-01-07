028a754be958c7374595c048d2481771e74524c8
saul gonzo <94.gonzlezsaul@gmail.com>
2023-01-07 11:48:05 -0500

    added index & destory route
    destory function was imported in the root

--- myViteReactApp/src/routes/root.jsx
+++ myViteReactApp/src/routes/root.jsx
@@ -1,9 +1,16 @@
-import { Outlet, Link, useLoaderData, Form } from 'react-router-dom'
+import {
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
+  useNavigation,
+} from 'react-router-dom'
 import { getContacts, createContact } from '../contacts'
 
 export async function action() {
   const contact = await createContact()
-  return { contact }
+  return redirect(`/contacts/${contact.id}/edit`)
 }
 
 export async function loader() {
@@ -13,6 +20,7 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData()
+  const navigation = useNavigation()
   return (
     <>
       <div id='sidebar'>
@@ -39,7 +47,12 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first}
@@ -49,7 +62,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{' '}
                     {contact.favorite && <span>*</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -60,7 +73,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id='detail'>
+      <div
+        id='detail'
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet />
       </div>
     </>
